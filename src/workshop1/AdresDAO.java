d18e159dcf44cd26caf15618cb90dd461ed0f9ec
Sonja <Sonja@Ramses>
2016-04-12 14:14:36 +0200

    readByAdresID toegevoegd

--- src/workshop1/AdresDAO.java
+++ src/workshop1/AdresDAO.java
@@ -11,7 +11,6 @@ import java.util.ArrayList;
 public class AdresDAO {
     
     static PreparedStatement stmnt;
-    Connection connection;
 
     public void createAdres(Adres adres) throws SQLException {
         String query = "INSERT straatnaam, huisnummer, toevoeging, postcode,"
@@ -19,14 +18,14 @@ public class AdresDAO {
         try {
             Connection connection = new DBConnector().getConnection();
             Class.forName("com.mysql.jdbc.Driver");
-            PreparedStatement stmnt = connection.prepareStatement(query);
+            PreparedStatement stmntCA = connection.prepareStatement(query);
             
-            stmnt.setString(1, adres.getStraatnaam());
-            stmnt.setInt(2, adres.getHuisnummer());
-            stmnt.setString(3, adres.getToevoeging());
-            stmnt.setString(4, adres.getPostcode());
-            stmnt.setString(5, adres.getWoonplaats());
-            stmnt.setInt(6, adres.getAdres_id());
+            stmntCA.setString(1, adres.getStraatnaam());
+            stmntCA.setInt(2, adres.getHuisnummer());
+            stmntCA.setString(3, adres.getToevoeging());
+            stmntCA.setString(4, adres.getPostcode());
+            stmntCA.setString(5, adres.getWoonplaats());
+            stmntCA.setInt(6, adres.getAdres_id());
             
         }
         catch (ClassNotFoundException | SQLException ex) {
@@ -41,15 +40,15 @@ public class AdresDAO {
     
     public ArrayList<Adres> readAdres() throws SQLException {
         String query = "SELECT straatnaam, huisnummer, toevoeging, postcode,"
-                + "woonplaats, adres_id, klant_id FROM adres";
+                + "woonplaats, adres_id FROM adres";
         ArrayList<Adres> lijst = new ArrayList<>();
         Adres adres;
         ResultSet rs;
         try {
             Connection connection = new DBConnector().getConnection(); 
             Class.forName("com.mysql.jdbc.Driver");
-            PreparedStatement stmnt = connection.prepareStatement(query);
-            rs = stmnt.executeQuery(query);
+            PreparedStatement stmntRA = connection.prepareStatement(query);
+            rs = stmntRA.executeQuery(query);
             while (rs.next()) {
                 adres = new Adres();
                 
@@ -59,7 +58,6 @@ public class AdresDAO {
                 adres.setPostcode(rs.getString("postcode"));
                 adres.setWoonplaats(rs.getString("woonplaats"));
                 adres.setAdres_id(rs.getInt("adres_id"));
-                adres.setKlant_id(rs.getInt("klant_id"));
                 
                 lijst.add(adres);
             }
@@ -76,19 +74,54 @@ public class AdresDAO {
         return lijst;
     }
     
+    public ArrayList<Adres> readAdresByID(int adresID) throws SQLException {
+        String query = "SELECT * FROM adres WHERE adres_id=?";
+        ArrayList<Adres> adresGegevens = new ArrayList<>();
+    
+        try {
+            Connection connection = new DBConnector().getConnection(); 
+            Class.forName("com.mysql.jdbc.Driver");
+            PreparedStatement stmntRAID = connection.prepareStatement(query);
+            stmntRAID.setInt(1, adresID);     
+            ResultSet rs = stmntRAID.executeQuery(query);
+            while (rs.next()) {
+                Adres adres = new Adres();
+                
+                adres.setStraatnaam(rs.getString("straatnaam"));
+                adres.setHuisnummer(rs.getInt("huisnummer"));
+                adres.setToevoeging(rs.getString("toevoeging"));
+                adres.setPostcode(rs.getString("postcode"));
+                adres.setWoonplaats(rs.getString("woonplaats"));
+                adres.setAdres_id(rs.getInt("adres_id"));
+                
+                adresGegevens.add(adres);
+            }
+        }
+        catch (ClassNotFoundException | SQLException ex) {
+            System.out.println("Probeer opnieuw.");
+            }
+        
+        finally {
+            stmnt.executeUpdate();
+            stmnt.close();
+        }
+        
+        return adresGegevens;
+    }
+    
     public void updateAdres(Adres adres) throws SQLException {
         String query = "UPDATE adres SET straatnaam, huisnummer, toevoeging"
                 + "postcode, woonplaats";
         
         try {
             Connection connection = new DBConnector().getConnection();
-            PreparedStatement stmnt = connection.prepareStatement(query);
+            PreparedStatement stmntUA = connection.prepareStatement(query);
             
-            stmnt.setString(1, adres.getStraatnaam());
-            stmnt.setInt(2, adres.getHuisnummer());
-            stmnt.setString(3, adres.getToevoeging());
-            stmnt.setString(4, adres.getPostcode());
-            stmnt.setString(5, adres.getWoonplaats());
+            stmntUA.setString(1, adres.getStraatnaam());
+            stmntUA.setInt(2, adres.getHuisnummer());
+            stmntUA.setString(3, adres.getToevoeging());
+            stmntUA.setString(4, adres.getPostcode());
+            stmntUA.setString(5, adres.getWoonplaats());
             
         }
         catch (SQLException | ClassNotFoundException ex) {
@@ -101,16 +134,16 @@ public class AdresDAO {
         }
     }
     
-    public void deleteAdres(int klant_id) {
+    public void deleteAdres(int adres_id) {
         String query = "DELETE straatnaam, huisnummer, toevoeging, postcode,"
-                + "woonplaats FROM adres WHERE klant_id=?";
+                + "woonplaats FROM adres WHERE adres_id=?";
         try {
             Connection connection = new DBConnector().getConnection();
             stmnt = connection.prepareStatement(query);
             
-            stmnt.setInt(1, klant_id);
+            stmnt.setInt(1, adres_id);
             
-        } catch (Exception ex) {
+        } catch (SQLException | ClassNotFoundException ex) {
             System.out.println("Probeer opnieuw.");
         }
     }
