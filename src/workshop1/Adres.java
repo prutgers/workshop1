d6638824649efe5515063025465a5a7561d51e8c
Sonja <Sonja@Ramses>
2016-04-12 08:34:49 +0200

    klant_id, adres_id toegevoegd

--- src/workshop1/Adres.java
+++ src/workshop1/Adres.java
@@ -11,6 +11,7 @@ public class Adres {
      String postcode;
      String woonplaats;
      int adres_id;
+     private int klant_id;
 
     /**
      * @return the straatnaam
@@ -94,6 +95,20 @@ public class Adres {
      */
     public void setAdres_id(int adres_id) {
         this.adres_id = adres_id;
+    }
+
+    /**
+     * @return the klant_id
+     */
+    public int getKlant_id() {
+        return klant_id;
+    }
+
+    /**
+     * @param klant_id the klant_id to set
+     */
+    public void setKlant_id(int klant_id) {
+        this.klant_id = klant_id;
     }
     
 }
