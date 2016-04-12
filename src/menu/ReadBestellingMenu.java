27a21c76aca7ddae16147f1d9c0e9f07ab2de8ee
Peter <Peter@192.168.2.3>
2016-04-12 12:05:10 +0200

    Merge origin/master
    Conflicts:
    	src/workshop1/Klant.java

---
+++ src/menu/ReadBestellingMenu.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package menu;
+
+
+
+import workshop1.*;
+
+
+/**
+ *
+ * @author Peter
+ */
+public class ReadBestellingMenu {
+   public static void startMenu(){
+    
+    } 
+}
