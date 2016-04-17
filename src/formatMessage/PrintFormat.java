da845167641864f6208e4aae033d21dd77c6605d
Herman de Jonge <herman.de.jonge@gmail.com>
2016-04-17 20:57:20 +0200

    aanpassingen om bestellingen en artikelen te koppelen. + test voor bestellingDAO

---
+++ src/formatMessage/PrintFormat.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package formatMessage;
+
+/**
+ *
+ * @author Gebruiker
+ */
+public class PrintFormat {
+    public static void printHeader(String header){
+        int spaces = (36/2) - (header.length()/2);
+        System.out.println("--------------------------------------");
+        System.out.print("|");
+        //print leading spaces
+        for(int i = 0;i<spaces;i++){
+            System.out.print(" ");
+        }
+        System.out.print(header);
+        
+        //print tracing spaces
+        for(int i = 0;i<spaces;i++){
+            System.out.print(" ");
+        }
+        System.out.print("|");
+        System.out.println("\n--------------------------------------");
+    }
+    
+}
