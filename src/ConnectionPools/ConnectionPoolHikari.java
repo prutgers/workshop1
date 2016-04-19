7118641e2ae3fbe6d245e9f4a5a84376c2ef4ad5
lucas <lucas@curie.schwartzenberg.xs4all.nl>
2016-04-19 18:59:37 +0200

    Merge origin/master
    Conflicts:
    	.gitignore

--- src/ConnectionPools/ConnectionPoolHikari.java
+++ src/ConnectionPools/ConnectionPoolHikari.java
@@ -6,6 +6,8 @@
 package ConnectionPools;
 
 import com.zaxxer.hikari.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -28,7 +30,10 @@ import java.sql.SQLException;
  * @author Peter
  */
 
-public class ConnectionPoolHikari {
+public class ConnectionPoolHikari implements GetConnection,java.io.Closeable  {
+    
+    static Logger logger = LoggerFactory.getLogger(ConnectionPoolC3P0.class);
+    
     HikariDataSource ds;
     public ConnectionPoolHikari(){
         HikariConfig config = new HikariConfig();
@@ -37,15 +42,25 @@ public class ConnectionPoolHikari {
         config.setInitializationFailFast(true);
         //config.setConnectionTestQuery("VALUES 1");
         config.setDataSourceClassName("com.mysql.jdbc.jdbc2.optional.MysqlDataSource");
+            logger.info("Hikari Driver loaded");
+            
         config.addDataSourceProperty("serverName", "localhost");
         config.addDataSourceProperty("port", "3306");
         config.addDataSourceProperty("databaseName", "workshopdb");
         config.addDataSourceProperty("user", "rsvier");
         config.addDataSourceProperty("password", "tiger");
+            logger.info("Database connected");
         
                 this.ds = new HikariDataSource(config);
     }
-    public Connection getConnection ()throws SQLException, ClassNotFoundException{
+    
+    @Override
+    public Connection getConnection() throws SQLException, ClassNotFoundException{
         return ds.getConnection();
+    }
+    
+    @Override
+    public void close(){
+     ds.close();
     }
 }
